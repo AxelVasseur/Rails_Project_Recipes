6c7f1349ea44cd843d76f6a95f881d8621abec20
Axel Vasseur <ar.vasseur@outlook.fr>
2022-06-29 23:37:23 +0200

    updated users, created register and login forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_162553) do
+ActiveRecord::Schema.define(version: 2022_06_29_205320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_162553) do
     t.string "lastname"
     t.string "login"
     t.string "password"
+    t.string "password_digest"
   end
 
   add_foreign_key "fridges", "users"
